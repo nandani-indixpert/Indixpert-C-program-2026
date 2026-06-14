dcfde829e9ac33d732f5363c02e3ad6dcda8b353
Nandani Kumari <nandanikumariii2329@gmail.com>
2026-06-14 21:31:50 +0530

    added sum

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int number1 = 10;
+    int number2 = 20;
+    int sum;
+    sum = number1 + number2;
+
+    printf("Sum of number1 and number2 = %d\n", sum);
+    return 0;
+}
