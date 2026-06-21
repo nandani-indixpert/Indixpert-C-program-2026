3888e8ce788b13eded7b771cd5466b4933d1ad49
nandani-indixpert <nandanikumariii2329@gmail.com>
2026-06-21 20:54:16 +0530

    Add files via upload

---
+++ name_list.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+int main()
+
+{
+    printf("\nHello! This is Nandani Kumari\n");
+    printf("From: Bihar(India)\n");
+    printf("\n========================================================\n");
+    printf("  ************ INDIXPERT STUDENTS LIST **************     \n");
+    printf("========================================================\n\n");
+    printf("1. Nandani Kumari\n");
+    printf("2. Saniya Kumari\n");
+    printf("3. Soni Kumari\n");
+    printf("4. Neha Kumari\n");
+    printf("5. Anjana Divakar\n");
+    printf("6. Rohit Kumar\n");
+    printf("7. Ankur Singh\n");
+    printf("8. Rajmani Kumar\n");
+    printf("9. Aayan Solanki\n");
+    printf("10. Himanshu Khetwal\n");
+    printf("11. Suraj\n");
+    printf("12. Garima Bhati\n");
+    printf("13. Anamika Nagarkoti\n");
+    printf("14. Himanshu Rathore\n");
+    printf("15. Deepak Singh Khetwal\n");
+    printf("16. Rahul Kumar\n");
+    printf("17. Md. Raazaullah\n");
+    printf("18. Shahid Alvi\n");
+    printf("19. Dolly Kumari\n");
+    printf("20. Aman Ali\n");
+    printf("21. Talat Fatima\n");
+    printf("22. Mayank Kumar\n");
+    printf("23. Ritika Kumari\n");
+    printf("***********************************************************\n\n");
+    getchar();
+    
+    
+    return 0;
+}
